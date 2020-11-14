e86fc1da7f9e6110b768939f00130b6d8f7ccf2e
calum boustead <calumboustead@calums-mbp.mynet>
2020-11-14 17:31:42 +0000

    Spring boot starter

---
+++ demoMQJMS/src/main/java/com/bousteca/demoMQJMS/DemoMqjmsApplication.java
@@ -0,0 +1,13 @@
+package com.bousteca.demoMQJMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMqjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMqjmsApplication.class, args);
+	}
+
+}
---
+++ demoMQJMS/src/test/java/com/bousteca/demoMQJMS/DemoMqjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bousteca.demoMQJMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMqjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
